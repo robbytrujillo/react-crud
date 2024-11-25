05daec7566f1140b05f20f651b5801f4b349a178
robbytrujillo <robbyikusuma88@gmail.com>
2024-11-25 14:43:00 +0700

    Introduction react-crud | delete

--- src/views/posts/edit.jsx
+++ src/views/posts/edit.jsx
@@ -1,76 +1,78 @@
-// import useState
+//import useState
 import { useState, useEffect } from 'react';
 
-// import useNavigation
+//import useNavigate
 import { useNavigate, useParams } from 'react-router-dom';
 
-// import API
+//import API
 import api from '../../api';
 
 export default function PostEdit() {
 
-    // define state
-    const [ image, setImage ] = useState('');
-    const [ title, setTitle ] = useState('');
-    const [ content, setContent ] = useState('');
+    //define state
+    const [image, setImage] = useState('');
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
 
-    // state validation
-    const [ errors, setErrors ] = useState([]);
+    //state validation
+    const [errors, setErrors] = useState([]);
 
-    // useNavigate
+    //useNavigate
     const navigate = useNavigate();
 
-    // destruct ID
+    //destruct ID
     const { id } = useParams();
 
-    // method fetchDetailPost
+    //method fetchDetailPost
     const fetchDetailPost = async () => {
-
-        // fetch data
+        
+        //fetch data
         await api.get(`/api/posts/${id}`)
             .then(response => {
-
-                // assign to state
+                
+                //assign to state
                 setTitle(response.data.data.title);
                 setContent(response.data.data.content);
             })
     }
 
-    // hook useEffect
+    //hook useEffect
     useEffect(() => {
-
-        // call method "fetchDetailPost"
+        
+        //call method "fetchDetailPost"
         fetchDetailPost();
+
     }, []);
 
-    // method handle file change
+    //method handle file change
     const handleFileChange = (e) => {
         setImage(e.target.files[0]);
     }
 
-    // method update post
+    //method update post
     const updatePost = async (e) => {
         e.preventDefault();
-
-        // init FormatData
+        
+        //init FormData
         const formData = new FormData();
 
-        // append data
+        //append data
         formData.append('image', image);
         formData.append('title', title);
         formData.append('content', content);
-        formData.append('_method', 'PUT');
+        formData.append('_method', 'PUT')
 
-        // send data with API
-        await api.put(`/api/posts/${id}`, formData)
+        //send data with API
+        await api.post(`/api/posts/${id}`, formData)
             .then(() => {
-
-                // redirect to posts index
+                
+                //redirect to posts index
                 navigate('/posts');
+
             })
             .catch(error => {
-
-                // set errors response to state "errors"
+                
+                //set errors response to state "errors"
                 setErrors(error.response.data);
             })
     }
@@ -81,23 +83,23 @@ export default function PostEdit() {
                 <div className="col-md-12">
                     <div className="card border-0 rounded shadow">
                         <div className="card-body">
-                            <form onSumbit={updatePost}>
-
-                                <div className='mb-3'>
+                            <form onSubmit={updatePost}>
+                            
+                                <div className="mb-3">
                                     <label className="form-label fw-bold">Image</label>
-                                    <input type="file" onChange={handleFileChange} className='form-control' />
+                                    <input type="file" onChange={handleFileChange} className="form-control"/>
                                     {
                                         errors.image && (
-                                            <div className='alert alert-danger mt-2'>
+                                            <div className="alert alert-danger mt-2">
                                                 {errors.image[0]}
                                             </div>
                                         )
                                     }
                                 </div>
 
-                                <div className='mb-3'>
+                                <div className="mb-3">
                                     <label className="form-label fw-bold">Title</label>
-                                    <input type='text' className='form-control' value={title} onChange={(e) => setTitle(e.target.value)} placeholder='Title Post' />
+                                    <input type="text" className="form-control" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title Post"/>
                                     {
                                         errors.title && (
                                             <div className="alert alert-danger mt-2">
@@ -106,10 +108,10 @@ export default function PostEdit() {
                                         )
                                     }
                                 </div>
-                                
-                                <div className='mb-3'>
+
+                                <div className="mb-3">
                                     <label className="form-label fw-bold">Content</label>
-                                    <textarea className='form-control' value={content} onChange={(e) => setContent(e.targer.value)} rows='5' placeholder='Content Post' />
+                                    <textarea className="form-control" value={content} onChange={(e) => setContent(e.target.value)} rows="5" placeholder="Content Post"></textarea>
                                     {
                                         errors.content && (
                                             <div className="alert alert-danger mt-2">
@@ -119,10 +121,8 @@ export default function PostEdit() {
                                     }
                                 </div>
 
-                                <button type='submit' className='btn btn-md btn-primary rounded-sm shadow border-0'>Update</button>
-
+                                <button type="submit" className="btn btn-md btn-primary rounded-sm shadow border-0">Update</button>
                             </form>
-                            {/* HALAMAN POST EDIT */}
                         </div>
                     </div>
                 </div>
