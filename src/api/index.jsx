d1e55716fdeb53d650c8a91e344c7893ed759aef
robbytrujillo <robbyikusuma88@gmail.com>
2024-11-25 11:20:13 +0700

    Introduction react-crud

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const Api = axios.create({
     // set default endpoint API
-    baseURL: 'https://localhost:8000',
+    baseURL: 'http://localhost:8000',
 })
 
 export default Api
